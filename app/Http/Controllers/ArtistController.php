958bf2e7789202fcb45c38c708c4ab8b18ad75b4
souad <riouch.souad@gmail.com>
2024-03-07 15:27:33 +0100

    Form update Artist

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -47,7 +47,11 @@ class ArtistController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $artist = Artist::find($id);
+        
+        return view('artist.edit',[
+            'artist' => $artist,
+        ]);
     }
 
     /**
@@ -55,7 +59,23 @@ class ArtistController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //Validation des données du formulaire
+        $validated = $request->validate([
+            'firstname' => 'required|max:60',
+            'lastname' => 'required|max:60',
+        ]);
+
+	   //Le formulaire a été validé, nous récupérons l’artiste à modifier
+        $artist = Artist::find($id);
+
+	   //Mise à jour des données modifiées et sauvegarde dans la base de données
+        $artist->update($validated);
+
+        return view('artist.show',[
+            'artist' => $artist,
+        ]);
+    
+
     }
 
     /**
---
+++ resources/views/artist/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Modifier un artiste')
+
+@section('content')
+    <h2>Modifier un artiste</h2>
+
+    <form action="{{ route('artist.update' ,$artist->id) }}" method="post">
+        @csrf
+        @method('PUT')
+        <div>
+            <label for="firstname">Prénom</label>
+            <input type="text" id="firstname" name="firstname">
+        </div>
+
+        <div>
+            <label for="lastname">Nom</label>
+            <input type="text" id="lastname" name="lastname">
+        </div>
+
+        <button>Modifier</button>
+   <a href="{{ route('artist.show',$artist->id) }}">Annuler</a>
+    </form>
+
+    <nav><a href="{{ route('artist.index') }}">Retour à l'index</a></nav>
+@endsection
