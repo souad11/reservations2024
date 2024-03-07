0234ab4d22578760a041ade61697ad7ab9a19e60
souad <riouch.souad@gmail.com>
2024-03-07 15:09:16 +0100

    Controller artist + templates + routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArtistController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+		->where('id', '[0-9]+')->name('artist.show');
