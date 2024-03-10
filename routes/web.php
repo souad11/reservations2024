f31c5c2952d868756305080203a392a9e04aa49b
souad <riouch.souad@gmail.com>
2024-03-10 12:10:07 +0100

    Breeze correction regression

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,16 +19,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
-Route::get('/artist/{id}', [ArtistController::class, 'show'])
-		->where('id', '[0-9]+')->name('artist.show');
-Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])
-        ->where('id', '[0-9]+')->name('artist.edit');
-Route::put('artist/{id}', [ArtistController::class, 'update'])
-        ->where('id', '[0-9]+')->name('artist.update');
-Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
-Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
 Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])
 	->where('id', '[0-9]+')->name('artist.delete');
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+        ->where('id', '[0-9]+')->name('artist.show');
+Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
+Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
+Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])
+    ->where('id', '[0-9]+')->name('artist.edit');
+Route::put('/artist/{id}', [ArtistController::class, 'update'])
+    ->where('id', '[0-9]+')->name('artist.update');
 
-        
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
