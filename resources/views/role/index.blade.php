22f64cc4ee0e262bcb0173152871cdf8e1721d24
souad <riouch.souad@gmail.com>
2024-03-19 13:31:55 +0100

    Controller Role +templates +routes

---
+++ resources/views/role/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+
+@section('title', 'Liste des roles')
+
+@section('content')
+    <h1>Liste des {{ $resource }}</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Role</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($roles as $role)
+            <tr>
+                <td>{{ $role->role }}</td>
+                <td>
+                    <a href="{{ route('role.show', $role->id) }}">Show</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\TypeController;
+use App\Http\Controllers\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,5 +48,13 @@ Route::middleware('auth')->group(function () {
 Route::get('/type', [TypeController::class, 'index'])->name('type.index');
 Route::get('/type/{id}', [TypeController::class, 'show'])
         ->where('id', '[0-9]+')->name('type.show');
+
+
+
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
+Route::get('/role/{id}', [RoleController::class, 'show'])
+        ->where('id', '[0-9]+')->name('role.show');
+
+
 
 require __DIR__.'/auth.php';
